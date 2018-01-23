3a104426ee2bd79fc79e522f087533fb774fc4a9
Yi Wang <wyia@microsoft.com>
2018-01-23 16:50:18 +0800

    chatbox

---
+++ intelligence-LUIS/Dialogs/RootLuisDialog.cs
@@ -0,0 +1,331 @@
+﻿//#define useSampleModel
+
+namespace LuisBot.Dialogs
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+    using System.Threading.Tasks;
+    using System.Web;
+    using Microsoft.Bot.Builder.Dialogs;
+    using Microsoft.Bot.Builder.FormFlow;
+    using Microsoft.Bot.Builder.Luis;
+    using Microsoft.Bot.Builder.Luis.Models;
+    using Microsoft.Bot.Connector;
+    using System.Net.Http;
+    using Newtonsoft.Json;
+    using Newtonsoft.Json.Linq;
+
+
+    [Serializable]
+#if useSampleModel
+    [LuisModel("162bf6ee-379b-4ce4-a519-5f5af90086b5", "11be6373fca44ded80fbe2afa8597c18")]
+#else
+   // [LuisModel("YourModelId", "YourSubscriptionKey")]
+    [LuisModel("7713e0ce-7ccc-4410-ab8a-39d532b4bfd7", "e5f6c0dea99e4ffb971fb0a8d887fb6b")]
+#endif
+    public class RootLuisDialog : LuisDialog<object>
+    {
+        private const string EntityGeographyCity = "builtin.geography.city";
+
+        private const string EntityHotelName = "Hotel";
+
+        private const string EntityAirportCode = "AirportCode";
+
+        private IList<string> titleOptions = new List<string> { "“Very stylish, great stay, great staff”", "“good hotel awful meals”", "“Need more attention to little things”", "“Lovely small hotel ideally situated to explore the area.”", "“Positive surprise”", "“Beautiful suite and resort”" };
+      //  http://169.254.193.149:5000/departments
+        const string rme280Uri = "http://169.254.193.149:5000/rme280";
+        const string ledurl = "http://169.254.193.149:5000/action/{0}";
+        //const string ledurl = "https://txccpete.p72.rt3.io/action/{0}";
+        //const string rme280Uri = "https://txccpete.p72.rt3.io/rme280";
+
+
+
+        string returnCode;
+        private async Task LEDAction(string actionCode)
+        {
+            string fullURI = string.Format(ledurl, actionCode);
+            returnCode = "-1";
+
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(fullURI).ConfigureAwait(false);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string result = await response.Content.ReadAsStringAsync();
+                        JObject o = JObject.Parse(result);
+                        returnCode = (string) o["Action"];
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.TraceError("Failed action on the LED: " + ex.ToString());
+            }
+        }
+
+        string temp;
+        private async Task CheckTemp()
+        {
+            // string fullURI = string.Format(rme280Uri);
+          //  rme280Uri = "http://localhost:5002/rme280";
+            temp = "not found";
+            try
+            {
+                using (var client = new HttpClient())
+                {
+                    HttpResponseMessage response = await client.GetAsync(rme280Uri).ConfigureAwait(false);
+                    if (response.IsSuccessStatusCode)
+                    {
+                        string result = await response.Content.ReadAsStringAsync();
+                        JObject o = JObject.Parse(result);
+                        temp = (string)o.SelectToken("sensor.Temp");
+                    }
+                }
+            }
+            catch (Exception ex)
+            {
+                System.Diagnostics.Trace.TraceError("Failed to get the temp value: " + ex.ToString());
+            }
+        }
+
+        [LuisIntent("TurnOnLED")]
+        public async Task TurnOnLED(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
+        {
+
+            string msg = "Done it!";
+            var message = await activity;
+            await this.LEDAction("0");
+            if(returnCode=="3")
+            {
+                msg = "the light is on already";
+            }
+            await context.PostAsync($"Sure, let me turn on the light for you now...'{msg}'");
+            context.Wait(this.MessageReceived);
+        }
+
+        [LuisIntent("TurnOffLED")]
+        public async Task TurnOffLED(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
+        {
+            string msg = "Done it!";
+            var message = await activity;
+            await this.LEDAction("1");
+            if (returnCode == "3")
+            {
+                msg = "the light is off already";
+            }
+            await context.PostAsync($"Sure, will do...'{msg}'");
+            context.Wait(this.MessageReceived);
+
+        }
+
+        [LuisIntent("ReadTemperature")]
+        public async Task ReadTemperature(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
+        {
+            string msg = "Sorry, cannot get the temperature now";
+            var message = await activity;
+            await CheckTemp();
+            if (temp != "not found")
+            {
+                msg = $"The room temperature is {temp}";
+            }
+            await context.PostAsync($"Sure, let me check...'{msg}'");
+            context.Wait(this.MessageReceived);
+
+        }
+
+
+
+        [LuisIntent("")]
+        [LuisIntent("None")]
+        public async Task None(IDialogContext context, LuisResult result)
+        {
+            string message = $"Sorry, I did not understand '{result.Query}'. Type 'help' if you need assistance.";
+
+            await context.PostAsync(message);
+
+            context.Wait(this.MessageReceived);
+        }
+
+
+
+
+        [LuisIntent("SearchHotels")]
+        public async Task Search(IDialogContext context, IAwaitable<IMessageActivity> activity, LuisResult result)
+        {
+
+            var message = await activity;
+            await context.PostAsync($"Welcome to the Hotels finder! we are analyzing your message: '{message.Text}'...");
+
+            var hotelsQuery = new HotelsQuery();
+
+            EntityRecommendation cityEntityRecommendation;
+
+            if (result.TryFindEntity(EntityGeographyCity, out cityEntityRecommendation))
+            {
+                cityEntityRecommendation.Type = "Destination";
+            }
+
+            var hotelsFormDialog = new FormDialog<HotelsQuery>(hotelsQuery, this.BuildHotelsForm, FormOptions.PromptInStart, result.Entities);
+
+            context.Call(hotelsFormDialog, this.ResumeAfterHotelsFormDialog);
+        }
+
+        [LuisIntent("ShowHotelsReviews")]
+        public async Task Reviews(IDialogContext context, LuisResult result)
+        {
+            EntityRecommendation hotelEntityRecommendation;
+
+            if (result.TryFindEntity(EntityHotelName, out hotelEntityRecommendation))
+            {
+                await context.PostAsync($"Looking for reviews of '{hotelEntityRecommendation.Entity}'...");
+
+                var resultMessage = context.MakeMessage();
+                resultMessage.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+                resultMessage.Attachments = new List<Attachment>();
+
+                for (int i = 0; i < 5; i++)
+                {
+                    var random = new Random(i);
+                    ThumbnailCard thumbnailCard = new ThumbnailCard()
+                    {
+                        Title = this.titleOptions[random.Next(0, this.titleOptions.Count - 1)],
+                        Text = "Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris odio magna, sodales vel ligula sit amet, vulputate vehicula velit. Nulla quis consectetur neque, sed commodo metus.",
+                        Images = new List<CardImage>()
+                        {
+                            new CardImage() { Url = "https://upload.wikimedia.org/wikipedia/en/e/ee/Unknown-person.gif" }
+                        },
+                    };
+
+                    resultMessage.Attachments.Add(thumbnailCard.ToAttachment());
+                }
+
+                await context.PostAsync(resultMessage);
+            }
+
+            context.Wait(this.MessageReceived);
+        }
+
+        [LuisIntent("Help")]
+        public async Task Help(IDialogContext context, LuisResult result)
+        {
+            // await context.PostAsync("Hi! Try asking me things like 'search hotels in Seattle', 'search hotels near LAX airport' or 'show me the reviews of The Bot Resort'");
+            await context.PostAsync("Hi, I am your house assistant. You may ask me to 'turn on/off the light' or check the room temperature.");
+
+            context.Wait(this.MessageReceived);
+        }
+
+        private IForm<HotelsQuery> BuildHotelsForm()
+        {
+            OnCompletionAsyncDelegate<HotelsQuery> processHotelsSearch = async (context, state) =>
+            {
+                var message = "Searching for hotels";
+                if (!string.IsNullOrEmpty(state.Destination))
+                {
+                    message += $" in {state.Destination}...";
+                }
+                else if (!string.IsNullOrEmpty(state.AirportCode))
+                {
+                    message += $" near {state.AirportCode.ToUpperInvariant()} airport...";
+                }
+
+                await context.PostAsync(message);
+            };
+
+            return new FormBuilder<HotelsQuery>()
+                .Field(nameof(HotelsQuery.Destination), (state) => string.IsNullOrEmpty(state.AirportCode))
+                .Field(nameof(HotelsQuery.AirportCode), (state) => string.IsNullOrEmpty(state.Destination))
+                .OnCompletion(processHotelsSearch)
+                .Build();
+        }
+
+        private async Task ResumeAfterHotelsFormDialog(IDialogContext context, IAwaitable<HotelsQuery> result)
+        {
+            try
+            {
+                var searchQuery = await result;
+
+                var hotels = await this.GetHotelsAsync(searchQuery);
+
+                await context.PostAsync($"I found {hotels.Count()} hotels:");
+
+                var resultMessage = context.MakeMessage();
+                resultMessage.AttachmentLayout = AttachmentLayoutTypes.Carousel;
+                resultMessage.Attachments = new List<Attachment>();
+
+                foreach (var hotel in hotels)
+                {
+                    HeroCard heroCard = new HeroCard()
+                    {
+                        Title = hotel.Name,
+                        Subtitle = $"{hotel.Rating} starts. {hotel.NumberOfReviews} reviews. From ${hotel.PriceStarting} per night.",
+                        Images = new List<CardImage>()
+                        {
+                            new CardImage() { Url = hotel.Image }
+                        },
+                        Buttons = new List<CardAction>()
+                        {
+                            new CardAction()
+                            {
+                                Title = "More details",
+                                Type = ActionTypes.OpenUrl,
+                                Value = $"https://www.bing.com/search?q=hotels+in+" + HttpUtility.UrlEncode(hotel.Location)
+                            }
+                        }
+                    };
+
+                    resultMessage.Attachments.Add(heroCard.ToAttachment());
+                }
+
+                await context.PostAsync(resultMessage);
+            }
+            catch (FormCanceledException ex)
+            {
+                string reply;
+
+                if (ex.InnerException == null)
+                {
+                    reply = "You have canceled the operation.";
+                }
+                else
+                {
+                    reply = $"Oops! Something went wrong :( Technical Details: {ex.InnerException.Message}";
+                }
+
+                await context.PostAsync(reply);
+            }
+            finally
+            {
+                context.Done<object>(null);
+            }
+        }
+
+        private async Task<IEnumerable<Hotel>> GetHotelsAsync(HotelsQuery searchQuery)
+        {
+            var hotels = new List<Hotel>();
+
+            // Filling the hotels results manually just for demo purposes
+            for (int i = 1; i <= 5; i++)
+            {
+                var random = new Random(i);
+                Hotel hotel = new Hotel()
+                {
+                    Name = $"{searchQuery.Destination ?? searchQuery.AirportCode} Hotel {i}",
+                    Location = searchQuery.Destination ?? searchQuery.AirportCode,
+                    Rating = random.Next(1, 5),
+                    NumberOfReviews = random.Next(0, 5000),
+                    PriceStarting = random.Next(80, 450),
+                    Image = $"https://placeholdit.imgix.net/~text?txtsize=35&txt=Hotel+{i}&w=500&h=260"
+                };
+
+                hotels.Add(hotel);
+            }
+
+            hotels.Sort((h1, h2) => h1.PriceStarting.CompareTo(h2.PriceStarting));
+
+            return hotels;
+        }
+    }
+}
---
+++ intelligence-LUIS/Sensor.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace LuisBot
+{
+    [Serializable]
+    public class Sensor
+    {
+        public string Pressure { get; set; }
+        public string Temp { get; set; }
+        public string Humidity { get; set; }
+    }
+}
